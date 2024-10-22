483358c5c502b05112d75249fe2950331f388f04
mahiboobnadaf <rasool97260@gmail.com>
2024-10-22 18:40:48 +0530

    Removed Border

--- redbus-app/src/Location_input.js
+++ redbus-app/src/Location_input.js
@@ -1,10 +1,11 @@
 function Location_input(){
+
     return (
         <div className="flex items-center justify-center h-[60%]"> 
-            <div className="text-red-950 p-10 bg-red-600 bg-opacity-30 rounded-2xl flex flex-wrap border-2 gap-2">
+            <div className="text-red-950 p-10 bg-opacity-40 rounded-2xl flex flex-wrap gap-2">
                 
                  {/* From Input */}
-                <div className="border-2 rounded-xl border-black p-2">
+                <div className=" p-2">
                     <label htmlFor="from" className=" block mb-1 text-white font-extrabold ">From:</label>
                     <input
                         type="text"
@@ -15,7 +16,7 @@ function Location_input(){
                 </div>
 
                 {/* To Input */}
-                <div className="border-2 border-black p-2 rounded-xl">
+                <div className=" p-2 ">
                     <label htmlFor="to" className="block mb-1 text-white font-extrabold">To:</label>
                     <input
                         type="text"
@@ -26,7 +27,7 @@ function Location_input(){
                 </div>
 
                 {/* Date Input */}
-                <div className="border-2 border-black p-2 rounded-xl">
+                <div className="p-2">
                     <label htmlFor="date" className="block mb-1 text-white font-extrabold">Date:</label>
                     <input
                         type="date"
@@ -36,7 +37,7 @@ function Location_input(){
                 </div>
 
                 {/* Search Button */}
-                <div className="border-2 border-black p-2 rounded-xl flex w-full justify-center">
+                <div className="p-2 flex w-full justify-center">
                     <button className="bg-slate-600 text-white font-bold px-4 py-2 rounded-xl hover:bg-slate-800">
                         Search
                     </button>
